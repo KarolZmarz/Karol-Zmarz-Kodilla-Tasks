3a12319edbb94221951dd40e0ebc01988747eb76
Karol Zmarz <karol.zmarz2@gmail.com>
2022-07-13 14:32:37 +0200

    first commit

---
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -0,0 +1,9 @@
+package com.crud.tasks;
+
+@SpringBootApplication
+public class TasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
+}
