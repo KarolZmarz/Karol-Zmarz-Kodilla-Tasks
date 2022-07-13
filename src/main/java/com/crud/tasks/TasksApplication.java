c2675227081673fee673b1236d96b357eac55983
Karol Zmarz <karol.zmarz2@gmail.com>
2022-07-13 15:17:29 +0200

    18.4 Mapping

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,9 +1,8 @@
 package com.crud.tasks;
 
-@SpringBootApplication
+
 public class TasksApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TasksApplication.class, args);
     }
 }
